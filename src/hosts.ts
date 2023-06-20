9eae755bacc5dc986b836b9b4f7fec331423424a
tp <61617660+ittp@users.noreply.github.com>
2023-06-21 01:24:30 +0300

    Initial commit

---
+++ src/hosts.ts
@@ -0,0 +1,13 @@
+let hosts = [
+  {
+    id: 1,
+    ndns: "a89d471dd2b8315004b8f248",
+    name: "AVT",
+    ip: "",
+    config: "a89d471dd2b8315004b8f248"
+  }
+];
+
+const zabbix = require("zabbix-rpc");
+
+module.exports = { hosts, zabbix };
